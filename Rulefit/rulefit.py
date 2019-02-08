17c02e55fa73c85020b5586b3c3b494c9624f846
Sambin <45697314+shengbin1992@users.noreply.github.com>
2019-02-08 01:57:35 -0600

    Update rulefit.py

--- Rulefit/rulefit.py
+++ Rulefit/rulefit.py
@@ -12,10 +12,10 @@ X1 = boston_data.drop("y", axis=1)
 features = X1.columns
 X1 = X1.as_matrix()
 
-X_train = X1[:795]
-y_train = y1[:795]
-X_test = X1[795:]
-y_test = y1[795:]
+X_train = X1[:500]
+y_train = y1[:500]
+X_test = X1[500:]
+y_test = y1[500:]
 
 typ='classifier' #regressor or classifier
 
@@ -27,12 +27,13 @@ if typ=='regressor':
             lin_standardise=True, exp_rand_tree_size=True,random_state=1) 
     rf.fit(X_train, y_train, feature_names=features)
     y_pred=rf.predict(X_test)
-    insample_rmse=np.sqrt(np.sum((y_pred-y_train)**2)/len(y_train))
+    insample_rmse=np.sqrt(np.sum((y_pred-y_test)**2)/len(y_test))
+    
 elif typ=='classifier':
     y_class=y_train.copy()
     y_class[y_class<1]=-1
     y_class[y_class>=1]=+1
-    N=X.shape[0]
+    N=X_train.shape[0]
     rf = RuleFit(tree_size=4,sample_fract='default',max_rules=2000,
                  memory_par=0.01,
                  tree_generator=None,
@@ -40,10 +41,34 @@ elif typ=='classifier':
                 lin_standardise=True, exp_rand_tree_size=True,random_state=1) 
     rf.fit(X_train, y_class, feature_names=features)
     y_pred=rf.predict(X_test)
-    insample_acc=len(y_pred==y_class)/len(y_class)
+    y_class1=y_test.copy()
+    y_class1[y_class1<1]=-1
+    y_class1[y_class1>=1]=+1
+    insample_acc=len(y_pred==y_class1)/len(y_class1)
 rules = rf.get_rules()
 
 rules = rules[rules.coef != 0].sort_values(by="support")
 num_rules_rule=len(rules[rules.type=='rule'])
 num_rules_linear=len(rules[rules.type=='linear'])
 print(rules)
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+fpr, tpr, _ = roc_curve(y_test, y_pred)
+
+# Calculate the AUC
+roc_auc = auc(fpr, tpr)
+print 'ROC AUC: %0.2f' % roc_auc
+
+# Plot of a ROC curve for a specific class
+plt.figure()
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend(loc="lower right")
+plt.show()
