b11d3b21a8a7f72cf0a0ef9ae80325d4f27b1bbb
Sambin <45697314+shengbin1992@users.noreply.github.com>
2019-02-11 19:16:38 -0600

    Update rulefit.py

--- models/rulefit.py
+++ models/rulefit.py
@@ -52,3 +52,24 @@ rules = rules[rules.coef != 0].sort_values(by="support")
 num_rules_rule=len(rules[rules.type=='rule'])
 num_rules_linear=len(rules[rules.type=='linear'])
 print(rules)
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+fpr, tpr, _ = roc_curve(y_test, y_pred)
+
+# Calculate the AUC
+roc_auc = auc(fpr, tpr)
+print 'ROC AUC: %0.2f' % roc_auc
+
+# Plot of a ROC curve for a specific class
+plt.figure()
+plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], 'k--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend(loc="lower right")
+plt.show()
